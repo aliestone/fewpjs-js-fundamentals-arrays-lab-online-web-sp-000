f6c8a15648673985dbaaeddbb3815971c7042c32
Alie Stone <helloaliestone@gmail.com>
2021-01-13 03:10:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Write your solution here!
 const list = ["Milo", "Otis", "Garfield"]
-const append = list 
+const append = list
 const prepend = list
 const removeLast = list
 const removeFirst = list
