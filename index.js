1c9ca36ba18db67926f536622b4ffca73296b8e5
Alie Stone <helloaliestone@gmail.com>
2021-01-13 02:56:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your solution here!
+const apend = ["Milo", "Otis", "Garfield"],
+prepend = ["Milo", "Otis", "Garfield"],
+removeLast = ["Milo", "Otis", "Garfield"].pop
+removeFirst = ["Milo", "Otis", "Garfield"];
+
