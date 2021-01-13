def031aa1cddf350cdb3191310d782c5eefd37a3
Alie Stone <helloaliestone@gmail.com>
2021-01-13 02:59:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,6 @@ const apend = ["Milo", "Otis", "Garfield"],
 prepend = ["Milo", "Otis", "Garfield"],
 removeLast = ["Milo", "Otis", "Garfield"].pop
 removeFirst = ["Milo", "Otis", "Garfield"];
+
+apend.push("Odie");
+prepend.unshift ("Odie");
